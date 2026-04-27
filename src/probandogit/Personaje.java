c89c72d2346c6586f05f181736225676dc96e051
Isabel Martin <isabel.marvel@iesjulianmarias.es>
2026-04-27 18:26:18 +0200

    Constructor y getters

---
+++ src/probandogit/Personaje.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package probandogit;
+
+/**
+ *
+ * @author isabel.marvel
+ */
+public class Personaje {
+    private String nombre;
+    private int fuerza;
+    private int edad;
+
+    public Personaje(String nombreIn, int fuerzaIn, int edadIn) {
+        this.nombre = nombreIn;
+        this.fuerza = fuerzaIn;
+        this.edad = edadIn;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getFuerza() {
+        return fuerza;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+    
+    
+    
+    
+    
+}
