48c1d45e40babca2798bcf137b7430a2cb4802f8
Isabel Martin <isabel.marvel@iesjulianmarias.es>
2026-04-27 18:28:45 +0200

    Clase hija mago

---
+++ src/probandogit/Mago.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package probandogit;
+
+/**
+ *
+ * @author isabel.marvel
+ */
+public class Mago extends Personaje {
+    
+    public Mago(String nombreIn, int fuerzaIn, int edadIn) {
+        super(nombreIn, fuerzaIn, edadIn);
+    }
+    
+}
