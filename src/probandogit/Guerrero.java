217b1d02f1719e6b40cd01c18bcb3bd7de8d20d0
Isabel Martin <isabel.marvel@iesjulianmarias.es>
2026-05-11 18:31:12 +0200

    revision 2

---
+++ src/probandogit/Guerrero.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package probandogit;
+
+/**
+ *
+ * @author isabel.marvel
+ */
+public class Guerrero extends Personaje{
+    
+    public Guerrero(String nombreIn, int fuerzaIn, int edadIn) {
+        super(nombreIn, fuerzaIn, edadIn);
+    }
+    
+}
